e648458ab22a457a15dd94599d224c586fa6d938
unknown <jonathums@gmail.com>
2023-04-03 20:20:29 -0500

    added release/debug variations to builds

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Download the following tools if you do not have them already installed.
 - Ninja: [download here](https://github.com/ninja-build/ninja/releases)
 - Cmake: [download here](https://cmake.org/download/)
 
-- Run the `ninja_build.bat` to build and run with Ninja.
+- Run the `ninja_build_debug.bat` or `ninja_build_release` to build and run with Ninja.
 - Run the `build.bat` to build and run with the default build system.
 
 
--- ninja_build_debug.bat
+++ ninja_build_debug.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set BUILD_DIR=build_ninja
+set BUILD_DIR=ninja_build_debug
 
 rem Create build directory if it does not exist
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
@@ -9,7 +9,7 @@ rem Change directory to build directory
 cd %BUILD_DIR%
 
 rem Generate build files using CMake
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G "Ninja" ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G "Ninja" ..
 
 rem Build application using generated build files
 ninja
